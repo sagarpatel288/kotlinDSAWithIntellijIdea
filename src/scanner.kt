b6ad575bba19e6596226e8b5fc69a6fe0a749f7f
sagarpatel288 <sagarpatel288@gmail.com>
2024-05-19 10:23:49 +0530

    Scanner.kt - Add comments

--- src/scanner.kt
+++ src/scanner.kt
@@ -1,8 +1,11 @@
 import java.util.Scanner
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    // To take the input, we use the `Scanner` class.
+    // We have to use the back-stick (``) around the `in` because the `in` is a reserved keyword in Kotlin.
+    // The back-stick works like escape or ignore kind of thing.
    val scanner = Scanner(System.`in`)
+    // The `scanner.nextInt()` expects an integer (a number without float/decimal) in the console as an input.
+    // We used `scanner.nextInt()` two times to take the input from the console two times.
     println(scanner.nextInt() + scanner.nextInt())
 }
