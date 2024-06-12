362a4d6edaa226abc6f3b7762b2017f81cd22012
sagarpatel288 <sagarpatel288@gmail.com>
2024-06-12 18:36:09 +0530

    Add a program that calculates and prints LCM (The Least Common Multiplier) using the Euclidean theory.

---
+++ src/level20Week2AlgorithmicWarmUp/080lcmUsingEuclideanTheory.kt
@@ -0,0 +1,31 @@
+package level20Week2AlgorithmicWarmUp
+
+import java.util.*
+
+fun main() {
+
+    /**
+     * Finding:
+     * GCD: Greatest Common Divisor or
+     * HCF: Highest Common Factor
+     *
+     * This is based on the Euclidean theory.
+     * According to the theory,
+     * gcd(a, b)
+     *
+     * = a if b is 0. Otherwise,
+     *
+     * = gcd(b, a % b) until the second argument becomes 0.
+     */
+    fun getGcd(first: Long, second: Long): Long {
+        if (second == 0L) return first
+        return getGcd(second, first % second)
+    }
+
+    val input = Scanner(System.`in`).nextLine().split(" ")
+    val first = input[0].toLong()
+    val second = input[1].toLong()
+    val gcd = getGcd(first, second)
+    val lcm = (first * second) / gcd
+    println(lcm)
+}
