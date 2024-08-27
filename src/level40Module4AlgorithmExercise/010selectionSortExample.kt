796ffcc613120176281c4768d1ab5666dd0f363b
sagarpatel288 <sagarpatel288@gmail.com>
2024-08-27 18:29:23 +0530

    Correct the last code (010selectionSortExample.kt) | Use input[minIndex] instead of input[i] to compare as the element has to be the smallest, not just small. | Also add the same comment.

--- src/level40Module4AlgorithmExercise/010selectionSortExample.kt
+++ src/level40Module4AlgorithmExercise/010selectionSortExample.kt
@@ -13,7 +13,8 @@ fun main() {
             for (j in i + 1..< input.size) {
                 // If we find that the element at the current index (i) is greater than the element at a particular index (j),
                 // then we conclude that the index j has a smaller value element than the element at index i.
-                if (input[j] < input[i]) {
+                // The key point here is, we compare input[j] with input[minIndex] instead of input[j] with input[i].
+                if (input[j] < input[minIndex]) {
                     // We find that j is the `minIndex`, the index which has a lower value element than the index i.
                     minIndex = j
                 }
