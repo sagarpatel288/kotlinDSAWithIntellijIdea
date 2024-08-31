f976ed803e0d5f988b198fbdb6f804f624dccf67
sagarpatel288 <sagarpatel288@gmail.com>
2024-08-31 15:52:05 +0530

    Improve (correct) logs to 020mergeSortExample.kt.

--- src/level40Module4AlgorithmExercise/020mergeSortExample.kt
+++ src/level40Module4AlgorithmExercise/020mergeSortExample.kt
@@ -25,7 +25,7 @@ fun main() {
             tempArray[currentIndexOfSortedArray++] = array[currentIndexOfLeft++]
         }
 
-        println(" :conquer: after the first while loop: before adding the remaining right part: tempArray: ${tempArray.toList()}")
+        println(" :conquer: after the second while loop: before adding the remaining right part: tempArray: ${tempArray.toList()}")
         while (currentIndexOfRight <= endIndex) {
             tempArray[currentIndexOfSortedArray++] = array[currentIndexOfRight++]
         }
