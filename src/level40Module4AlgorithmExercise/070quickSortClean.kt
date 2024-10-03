97a3249af7c321de561e2913d42377a2481bc125
sagarpatel288 <sagarpatel288@gmail.com>
2024-10-03 17:34:43 +0530

    Update the input for the 060quickSort.kt as per our Google Doc to be aligned.

---
+++ src/level40Module4AlgorithmExercise/070quickSortClean.kt
@@ -0,0 +1,38 @@
+package level40Module4AlgorithmExercise
+
+fun main() {
+
+    fun swapElements(input: IntArray, positionOne: Int, positionTwo: Int) {
+        val temp = input[positionOne]
+        input[positionOne] = input[positionTwo]
+        input[positionTwo] = temp
+    }
+
+    fun getPartitionIndex(input: IntArray, start: Int, end: Int): Int {
+        val pivot = input[end]
+        var partitionIndex = start - 1
+        for (j in start..<end) {
+            if (input[j] <= pivot) {
+                partitionIndex++
+                if (partitionIndex != j) {
+                    swapElements(input, partitionIndex, j)
+                }
+            }
+        }
+        partitionIndex++
+        swapElements(input, partitionIndex, end)
+        return partitionIndex
+    }
+
+    fun quickSort(input: IntArray, start: Int, end: Int) {
+        if (start < end) {
+            val partitionIndex =  getPartitionIndex(input, start, end)
+            quickSort(input, start, partitionIndex - 1)
+            quickSort(input, partitionIndex + 1, end)
+        }
+    }
+
+    val input = intArrayOf(-3, 8, -2, 1, 6, -5, 3, 4)
+    quickSort(input, 0, input.lastIndex)
+    println(": :main: sorted array: ${input.toList()}")
+}
