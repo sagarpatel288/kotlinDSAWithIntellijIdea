c0ebf6b31a4e031fc03025536de9f42caa053897
sagarpatel288 <sagarpatel288@gmail.com>
2024-10-21 17:43:51 +0530

    Improve comments to understand the 3-way partitioning (a.k.a. The Dutch National Flag) quicksort.

--- src/level40Module4AlgorithmExercise/110quickSort3wayPartitioning.kt
+++ src/level40Module4AlgorithmExercise/110quickSort3wayPartitioning.kt
@@ -23,12 +23,12 @@ fun main() {
      * The first marker, known as the blue marker, indicates the end, the last position of the color blue.
      * All the cards to the left of the blue marker should be of color blue.
      * The middle marker, known as the white marker, indicates the beginning of the color white.
-     * It can also indicate the first unknown value (card).
+     * It can also indicate the first unknown value (card) or the current position of an iteration.
      * The last marker, known as the red marker, indicates the beginning of the color red.
      * All the cards to the right of the red marker should be of color red.
      *
      * The initial array is: [unknown, unknown, unknown, unknown, unknown, unknown].
-     * We put the first and the middle marker at the beginning, at index 0 and the red marker in the end, at index 5.
+     * We put the first and the middle or current marker at the beginning, at index 0 and the red marker in the end, at index 5.
      * So, the blue marker is at index 0.
      * The white marker is at index 0.
      * The red marker is at index 5.
@@ -59,81 +59,87 @@ fun main() {
      * We don't swap anything if we find that the element belongs to the middle region.
      * The reason is, if we arrange the first and the last regions, the middle region should be arranged on its own!
      * (Magic!)!.
-     * However, the first card is not unknown now. Hence, we increase the middle marker.
-     * So, the middle marker is at index 1 now.
+     * However, the first card is not unknown now. Hence, we increase the middle or current marker.
+     * So, the middle or current marker is at index 1 now.
      * The blue marker is still at index 0.
      * The red marker is at index 3.
      *
      * Now, we reveal the card at index 1, and we find that it is a blue card.
      * So, we swap it with the current position of the blue marker, which is at index 0.
      * It means, at index 0, we get a blue card, and at index 1, we have a white card.
-     * We also increase the middle marker and the blue marker.
+     * We also increase the middle or current marker and the blue marker.
      * Because, the index 1 is not unknown anymore. We got the white card when we swapped it with the blue card.
      * And we get all the blue cards to the left of the blue marker.
      * So, the array becomes: [blue, white, unknown, unknown, red, red].
      * The blue marker is at index 1.
-     * The middle marker is at index 2.
+     * The middle or current marker is at index 2.
      * The red marker is at index 3.
      *
-     * The middle marker is at index 2. We reveal the card. We find that it is a blue card.
+     * The middle or current marker is at index 2. We reveal the card. We find that it is a blue card.
      * So, we swap it with the current position of the blue marker, which is at index 1.
-     * And we increase the middle marker because index 1 is not an unknown value anymore.
+     * And we increase the middle or current marker because index 1 is not an unknown value anymore.
      * And we increase the blue marker as well so that we get all the blue cards to the left of the blue marker.
      * So, the array becomes: [blue, blue, white, unknown, red, red].
      * The blue marker is at index 2.
-     * The middle marker is at index 3.
+     * The middle or current marker is at index 3.
      * The red marker is at index 3.
      *
-     * The middle marker is at index 3. We reveal the card. We find that it is a white card.
+     * The middle or current marker is at index 3. We reveal the card. We find that it is a white card.
      * The white card belongs to the middle region and if we find an element that belongs to the middle region,
      * we don't swap it with any other element, because we don't need to.
      * Because, when we arrange only the first and the last region, the middle region should be arranged on its own!
      * (Magic!)!.
-     * However, the card at index 3 is not unknown anymore. So, we increase the middle marker.
+     * However, the card at index 3 is not unknown anymore. So, we increase the middle or current marker.
      * So, the array becomes: [blue, blue, white, white, red, red].
      * The blue marker is at index 2.
      * The red marker is at index 3.
-     * The middle marker is at index ?. If you notice, once the middle marker crosses the red marker, there is no
+     * The middle or current marker is at index ?. If you notice, once the middle or current marker crosses the red marker, there is no
      * unknown value left!! We have already finished the iteration and sorted the cards!! (Magic!)!.
      *
-     * 1. We start with middle and blue markers at the first index (start) and the red marker at the last index (end).
+     * 1. We start with middle or current and blue markers at the first index (start) and the red marker at the last index (end).
      * 2. If we find that a card belongs to the last region, we swap it with the last card, we decrease the red marker.
      *    So that all the red cards stay to the right of the red marker.
-     *    However, we do not increase the middle or the blue marker.
+     *    However, we do not increase the middle or current or the blue marker.
      *    Because the card we got in exchange, is yet unknown.
-     * 3. If we find that a card belongs to the first region, we increase both the blue marker and the middle marker.
+     * 3. If we find that a card belongs to the first region, we increase both the blue marker and the middle or current marker.
      *    Because, the card is not unknown anymore, and all the blue card should stay to the left of the blue marker.
      * 4. If we find that a card belongs to the middle region, we don't swap it with any other card.
-     *    However, we increase the middle marker. Because, the card is not unknown anymore.
-     * 5. When we find that the middle marker crosses the red marker, we should stop the iteration.
+     *    However, we increase the middle or current marker. Because, the card is not unknown anymore.
+     * 5. When we find that the middle or current marker crosses the red marker, we should stop the iteration.
      */
     fun quickSort(input: IntArray, start: Int, end: Int) {
         if (start >= end) return
-        println(": :quickSort: input: before random pivot: ${input.toList()} start: $start end: $end")
+        println(": :quickSort: input: -----------------before random pivot: ${input.toList()} start: $start end: $end")
         val randomIndex = Random.nextInt(start, end + 1)
         swapElements(input, randomIndex, end)
         val pivot = input[end]
         var lessThanMarker = start
         var greaterThanMarker = end
         var currentPointer = start
-        println(": :quickSort: input: after random pivot: ${input.toList()} pivot: $pivot lessThan: $lessThanMarker greaterThan: $greaterThanMarker")
+        println(": :quickSort: input: -----------------after random pivot: ${input.toList()} pivot: $pivot lessThan: $lessThanMarker greaterThan: $greaterThanMarker")
         while (currentPointer <= greaterThanMarker) {
-            println(": :quickSort: input: loop: ${input.toList()} mid: $currentPointer lessThan: $lessThanMarker greaterThan: $greaterThanMarker start: $start end: $end")
+            println(": :quickSort: loop: input: ${input.toList()} current: $currentPointer greaterThan: $greaterThanMarker")
             when {
                 input[currentPointer] < pivot -> {
-                    println(": :quickSort: lessThan: $lessThanMarker input: ${input.toList()} mid: $currentPointer")
-                    swapElements(input, lessThanMarker, currentPointer)
+                    println(": :quickSort: lessThan: $lessThanMarker input: ${input.toList()} currentPointer: $currentPointer")
+                    if (currentPointer != lessThanMarker && input[currentPointer] != input[lessThanMarker]) {
+                        swapElements(input, lessThanMarker, currentPointer)
+                    }
                     lessThanMarker++
                     currentPointer++
+                    println(": :quickSort: after swap: lessThan: $lessThanMarker currentPointer: $currentPointer input: ${input.toList()}")
                 }
                 input[currentPointer] > pivot -> {
-                    println(": :quickSort: greaterThan: $greaterThanMarker input: ${input.toList()} mid: $currentPointer")
-                    swapElements(input, currentPointer, greaterThanMarker)
+                    println(": :quickSort: greaterThan: $greaterThanMarker input: ${input.toList()} currentPointer: $currentPointer")
+                    if (input[currentPointer] != input[greaterThanMarker]) {
+                        swapElements(input, currentPointer, greaterThanMarker)
+                    }
                     greaterThanMarker--
+                    println(": :quickSort: after swap: greaterThan: $greaterThanMarker currentPointer: $currentPointer input: ${input.toList()}")
                 }
                 else -> {
                     currentPointer++
-                    println(": :quickSort: mid: $currentPointer")
+                    println(": :quickSort: elseEqual: Now, current: $currentPointer")
                 }
             }
         }
