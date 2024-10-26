e5e8c1dc47202c4f9d2fd269b19db625f6119ea0
sagarpatel288 <sagarpatel288@gmail.com>
2024-10-26 08:24:40 +0530

    Add problem statement to 010MoneyChangeDenomination.kt

--- src/level30Module3AlgorithmExercise/010MoneyChangeDenomination.kt
+++ src/level30Module3AlgorithmExercise/010MoneyChangeDenomination.kt
@@ -1,5 +1,42 @@
 package level30Module3AlgorithmExercise
 
+/**
+ * Problem Statement:
+ *
+ * Money Change Problem:
+ *
+ * Compute the minimum number of coins needed to change the given value into coins with denominations 1, 5, and 10.
+ *
+ * Input: An integer money.
+ *
+ * Output: The minimum number of coins with denominations 1, 5, and 10 that changes money.
+ *
+ * In this problem, you will implement a simple greedy algorithm used by cashiers all over the world.
+ * We assume that a cashier has unlimited number of coins of each denomination.
+ *
+ * Input format. Integer money.
+ * Output format. The minimum number of coins with denominations 1, 5, 10 that changes money.
+ *
+ * Constraints:
+ * 1 ≤ money ≤ 10 raised to the power 3.
+ *
+ * Sample 1.
+ * Input:
+ * 2
+ * Output:
+ * 2
+ *
+ * 2 = 1 + 1.
+ *
+ * Sample 2.
+ * Input:
+ * 28
+ * Output:
+ * 6
+ *
+ * 28 = 10 + 10 + 5 + 1 + 1 + 1.
+ *
+ */
 fun main() {
 
     fun getMinimumCoinsNeeded(money: Long): Long {
