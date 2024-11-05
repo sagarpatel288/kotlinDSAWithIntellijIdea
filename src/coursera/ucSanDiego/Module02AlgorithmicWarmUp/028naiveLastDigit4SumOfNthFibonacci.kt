d48fd7d8dd0cc7492312311aad541b1d24a44243
sagarpatel288 <sagarpatel288@gmail.com>
2024-11-05 14:07:03 +0530

    Refactor: Create the coursera --> ucSanDiego packages and move relevant packages there. Also, rename the relevant packages to map, relate, and understand easily as per coursera's Algorithmic Toolbox by University Of California San Diego.

---
+++ src/coursera/ucSanDiego/Module02AlgorithmicWarmUp/028naiveLastDigit4SumOfNthFibonacci.kt
@@ -0,0 +1,208 @@
+package coursera.ucSanDiego.Module02AlgorithmicWarmUp
+
+/**
+ * Problem Statement:
+ *
+ * Last Digit of the Sum of Fibonacci Numbers Problem:
+ *
+ * Compute the last digit of F0 + F1 +···+ Fn.
+ *
+ * Input: An integer n.
+ *
+ * Output: The last digit of F0 + F1 + ···+ Fn.
+ *
+ *
+ * Input format. An integer n.
+ *
+ * Output format. (F0+F1+···+Fn) mod 10.
+ *
+ * Constraints. 0 ≤ n ≤10^14.
+ *
+ * Sample 1.
+ *
+ * Input:
+ *
+ * 3
+ *
+ * Output:
+ *
+ * 4
+ *
+ * F0+F1+F2+F3 =0+1+1+2=4.
+ * 4 % 10 = 4.
+ *
+ * Sample 2.
+ *
+ * Input:
+ *
+ * 100
+ *
+ * Output:
+ *
+ * 5
+ *
+ * F0+···+F100 =927372692193078999175 % 10 = 5.
+ *
+ *
+ */
+fun main() {
+
+    val nthFibonacci = readln().toInt()
+    val sumOfNthFibonacci = sumOfNthFibonacci(nthFibonacci, 10)
+    val sumOfNthFibonacciByModularAddition = sumOfNthFibonacciByModularAddition(nthFibonacci, 10)
+    val sumOfReducedNthFibonacci = sumOfReducedNthFibonacci(nthFibonacci, 10)
+    println(": :main: sumOfNthFibonacci: $sumOfNthFibonacci")
+    println(": :main: sumOfNthFibonacciByModularAddition $sumOfNthFibonacciByModularAddition")
+    println(": :main: sumOfReducedNthFibonacci $sumOfReducedNthFibonacci")
+}
+
+/**
+ * We want to find: `S(n) % m`
+ * However, Sum of S(n) = `F(n+2) - 1`
+ * So, `S(n) % m` = `(F(n+2) - 1) % m`
+ */
+fun sumOfNthFibonacci(nthFibonacci: Int, modulo: Int): Long {
+    // F(0)
+    var previous = 0L
+    // F(1)
+    var current = 1L
+    val fibonacciElements = mutableListOf(previous, current)
+    // S(n) = F(n+2) - 1. So, we iterate up to F(n+2) instead of n.
+    for (i in 2..(nthFibonacci + 2)) {
+        val newCurrent = previous + current
+        fibonacciElements.add(newCurrent)
+        previous = current
+        current = newCurrent
+    }
+    println("sumOfNthFibonacci: fibonacciElements: $fibonacciElements")
+    println(": :sumOfNthFibonacci: current: $current")
+    // S(n) = F(n+2) - 1. So, we subtract 1.
+    return (current - 1) % modulo
+}
+
+/**
+ * We want to find sum of Nth Fibonacci % modulo
+ *  = `S(n) % m`
+ *  Apply Sum of Nth Fibonacci rule/theory:
+ *  = `(F(n+2) - 1) % m`
+ *  Simplify F(n+2) as F(n')
+ *  = `(F(n') - 1) % m`
+ *  Apply Modular arithmetic theory:
+ *  [Modular Subtraction](https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/modular-addition-and-subtraction)
+ *  = `(F(n') % m - 1 % m) % m`
+ *  Each Fibonacci number is the sum of the two preceding ones.
+ *  = `((F(n'1) + F(n'2)) % m - 1 % m) % m`
+ *  Apply Modular arithmetic theory:
+ *  [Modular Addition](https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/modular-addition-and-subtraction)
+ *  = `((F(n'1) % m + F(n'2) % m) % m - 1 % m) % m`
+ */
+fun sumOfNthFibonacciByModularAddition(nthFibonacci: Int, modulo: Int): Long {
+    // F(0) % modulo
+    var previousRemainder = 0L
+    // F(1) % modulo
+    var nextRemainder = 1L
+    val fibonacciRemainderElements = mutableListOf(previousRemainder, nextRemainder)
+    for (i in 2..(nthFibonacci + 2)) {
+        // Each Fibonacci number is the sum of the two preceding ones.
+        // (A + B) mod C = (A mod C + B mod C) mod C
+        val newRemainder = (previousRemainder + nextRemainder) % modulo
+        fibonacciRemainderElements.add(newRemainder)
+        previousRemainder = nextRemainder
+        nextRemainder = newRemainder
+    }
+    println(": :sumOfNthFibonacciByModularAddition: fibonacciRemainderElements: $fibonacciRemainderElements")
+    println(": :sumOfNthFibonacciByModularAddition: (n+2) remainder: $nextRemainder")
+    // S(n) = F(n+2) - 1. So, we subtract 1.
+    // Key-point:
+    // We add 10 because if the remainder is 0, subtracting 1 will give a negative number - that we want to avoid.
+    // For example, the last digit so far we have calculated can be 0 at this moment.
+    // According to the formula, we would do, 0 - 1 = -1.
+    // We want to avoid the negative number. So, we add 10.
+    // Hence, it becomes: 0 - 1 + 10 = 9 % 10 = 9.
+    // Why do we do % 10 here at this stage also?
+    // Because, this is a common code. So, it applies even when we get 4 as a last digit after the above for loop.
+    // Now, when we do 4 - 1 + 10 = we get 13, and we do not want to return 13.
+    // Hence, the % 10 will make it: 13 % 10 = 3, the last digit we want.
+    // So, in the end, % 10 ensures that we get the last digit.
+    return (nextRemainder - 1 + 10) % 10
+}
+
+/**
+ * We want to find sum of Nth Fibonacci % modulo
+ *  = `S(n) % m`
+ *  Apply Sum of Nth Fibonacci rule/theory:
+ *  = `(F(n+2) - 1) % m`
+ *  Simplify F(n+2) as F(n')
+ *  = `(F(n') - 1) % m`
+ *  When we have a Fibonacci number and a modulo to apply, we use the Pisano period.
+ *  According to the Pisano period theory, find the much smaller congruence number.
+ *  Because the modulo on the original large Fibonacci number and the modulo on the much smaller congruence number,
+ *  is the same.
+ *  = `(F(p) - 1) % m`
+ *  Apply Modular arithmetic theory:
+ *  [Modular Subtraction](https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/modular-addition-and-subtraction)
+ *  = `(F(p) % m - 1 % m) % m`
+ *  Each Fibonacci number is the sum of the two preceding ones.
+ *  = `((F(p1) + F(p2)) % m - 1 % m) % m`
+ *  Apply Modular arithmetic theory:
+ *  [Modular Addition](https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/modular-addition-and-subtraction)
+ *  = `((F(p1) % m + F(p2) % m) % m - 1 % m) % m`
+ */
+fun sumOfReducedNthFibonacci(nthFibonacci: Int, modulo: Int): Long {
+    // When we have a Fibonacci number and a modulo to apply, we use the Pisano period.
+    // According to the Pisano period theory, find the much smaller congruence number.
+    // Because the modulo on the original large Fibonacci number and the modulo on the much smaller congruence number,
+    // are the same.
+    val pisanoPeriod = findPisanoPeriodOfFibonacci(modulo)
+    val reducedN = (nthFibonacci + 2) % pisanoPeriod
+    println(": :sumOfReducedNthFibonacci: pisanoPeriod: $pisanoPeriod reducedN: $reducedN")
+    // F(0) % modulo
+    var previousRemainder = 0L
+    // F(1) % modulo
+    var nextRemainder = 1L
+    val reducedRemainderElements = mutableListOf(previousRemainder, nextRemainder)
+    for (i in 2..reducedN) {
+        val newRemainder = (previousRemainder + nextRemainder) % modulo
+        reducedRemainderElements.add(newRemainder)
+        previousRemainder = nextRemainder
+        nextRemainder = newRemainder
+    }
+    println(": :sumOfReducedNthFibonacci: reducedRemainderElements: $reducedRemainderElements")
+    println(": :sumOfReducedNthFibonacci: (n+2) remainder: $nextRemainder")
+    // S(n) = F(n+2) - 1. So, we subtract 1.
+    // We add 10 because if the remainder is 0, subtracting 1 will give a negative number - that we want to avoid.
+    // For example, the last digit so far we have calculated can be 0 at this moment.
+    // According to the formula, we would do, 0 - 1 = -1.
+    // We want to avoid the negative number. So, we add 10.
+    // Hence, it becomes: 0 - 1 + 10 = 9 % 10 = 9.
+    // Why do we do % 10 here at this stage also?
+    // Because, this is a common code. So, it applies even when we get 4 as a last digit after the above for loop.
+    // Now, when we do 4 - 1 + 10 = we get 13, and we do not want to return 13.
+    // Hence, the % 10 will make it: 13 % 10 = 3, the last digit we want.
+    // So, in the end, % 10 ensures that we get the last digit.
+    return (nextRemainder - 1 + 10) % 10
+}
+
+fun findPisanoPeriodOfFibonacci(modulo: Int): Int {
+    // We are interested in tracking the pattern of remainders so that we can know when it repeats.
+    // F(0) % modulo
+    var previousRemainder = 0
+    // F(1) % modulo
+    var nextRemainder = 1
+    // This is to track at which point the cycle (sequence or pattern) of the remainders repeats.
+    var cycleLength = 0
+    // The Pisano period is always less than or equal to modulo squared.
+    for (i in 0..(modulo * modulo)) {
+        val newRemainder = (previousRemainder + nextRemainder) % modulo
+        previousRemainder = nextRemainder
+        nextRemainder = newRemainder
+        cycleLength++
+
+        // When we find that the cycle (sequence or pattern) of the remainders repeats itself,
+        // that is the length (point) of the Pisano period.
+        if (previousRemainder == 0 && nextRemainder == 1) {
+            return cycleLength
+        }
+    }
+    return cycleLength
+}
---
+++ src/coursera/ucSanDiego/Module04DivideAndConquer/010selectionSortExample.kt
@@ -0,0 +1,120 @@
+package coursera.ucSanDiego.Module04DivideAndConquer
+
+/**
+ * Problem Statement Example --------------------------------
+ *
+ * Explain and demonstrate (illustrate) selection sort:
+ * OR
+ * Explain and demonstrate a sorting algorithm that is an in-place algorithm and has minimum space-complexity.
+ * OR
+ * Explain and demonstrate a sorting algorithm that is memory efficient (but with poor time-complexity).
+ *
+ * -------------------------------- Explanation
+ *
+ * Selection Sort is a simple sorting algorithm that sorts an array by repeatedly finding the minimum element
+ * (considering ascending order) from the unsorted portion of the array and moving it to the beginning.
+ * The algorithm maintains two sub-arrays:
+ *
+ * The sub-array that is already sorted.
+ *
+ * The remaining sub-array that is unsorted.
+ *
+ * What do we do exactly?
+ *
+ * We compare each index with all the remaining indices.
+ * While comparing, if we find an index that has a lower value element,
+ * then we swap the positions of the elements so that the lower value element gets the lower index value
+ * to get the non-decreasing ascending order.
+ *
+ * For example, we start an iteration from 0 and go up to the last index of the input.
+ *
+ * We assume that the first index is the minimum index and holds the smallest value,
+ * which we compare with the rest of the indices.
+ *
+ * The comparison of i with the rest of the indices introduces a nested for loop from i + 1 up to the last index.
+ *
+ * If we find a smaller value than our minimum index, we take the minimum index label from the first index
+ * and assign it to this smaller index.
+ *
+ * We continue the iteration, performing the same action whenever we
+ * find an index that holds a value smaller than the minimum index.
+ *
+ * After the first iteration, if the minimum index is not equal to the current index of the iteration,
+ * we swap the values.
+ *
+ * We do this until we reach the last index.
+ *
+ * So, it's like we select the current index, assign it as the minimum index, compare it with the other elements,
+ * swap if necessary, and then select the next index. We do this up to the last index of the input.
+ *
+ * Time-Complexity:
+ *
+ * The time-complexity of the selection sort is O(n squared) for all 3 cases (best, average, and worst).
+ *
+ * Space-Complexity:
+ *
+ * It is an in-place algorithm. It means, it does not require additional space for another array,
+ * except a constant amount of extra space. Hence, the space-complexity of the selection sort is O(1).
+ *
+ * How does the space complexity of the selection sort is O(1)?
+ * Why does the space complexity of the selection sort is O(1)?
+ *
+ * The selection sort uses a temporary variable to store `minIndex` and a `temp` variable during the swapping process.
+ * Both these temporary variables do not depend on the original input size.
+ *
+ * Apart from these two temporary variables, the selection sort does not create any additional and critical memory
+ * that depends on the size of the original input.
+ *
+ * Hence, we can say that, the space complexity of the selection sort is O(1).
+ *
+ *
+ */
+fun main() {
+
+    fun selectionSort(input: MutableList<Int>) {
+        if (input.size <= 1) {
+            println(": :selectionSort: sorted: ${input.toList()}")
+            return
+        }
+        // Going through each index of the incoming list
+        for (i in input.indices) {
+            // Taking the current index and will compare it with the rest of the elements and indices.
+            // `minIndex` means, an index that has the lower value element than it's successive indices.
+            var minIndex = i
+
+            // Comparing the element at the current index with the element of each index
+            for (j in i + 1..< input.size) {
+                // If we find that the element at the current index (i) is greater than the element at a particular index (j),
+                // then we conclude that the index j has a smaller value element than the element at index i.
+                // The key point here is, we compare input[j] with input[minIndex] instead of input[j] with input[i].
+                if (input[j] < input[minIndex]) {
+                    // We find that j is the `minIndex`, the index which has a lower value element than the index i.
+                    minIndex = j
+                }
+            }
+
+            // Avoid unnecessary swapping
+            if (i != minIndex) {
+                // Get the element from the current index
+                val oldValue = input[i]
+                // Set the element at the current index from the element at the minIndex
+                input[i] = input[minIndex]
+                // Set the element of the current index at the index j.
+                input[minIndex] = oldValue
+            }
+        }
+        println(input)
+    }
+
+    /*val inputArray = readln().split(" ").map {
+        it.toInt()
+    }.toMutableList()*/
+
+    fun getInput(): IntArray {
+        return intArrayOf(-5, -3, -4, -5, 1, 0, 1, 0, 2, 1)
+    }
+
+    val input = getInput()
+
+    selectionSort(input.toMutableList())
+}
