d7042481bfbef314d7f4eb0051c6fa6012443373
sagarpatel288 <sagarpatel288@gmail.com>
2025-01-24 15:07:11 +0530

    Add backtracking (reconstruction) solution to the 020coinChangeDynamicProgram.kt of the coursera's algorithmic toolbox's module 05 Dynamic Programming, programming assignment 01, problem number 01 | Money change using the dynamic programming.

--- src/coursera/ucSanDiego/module05DynamicProgramming/module05ProgrammingAssignment01/020coinChangeDynamicProgram.kt
+++ src/coursera/ucSanDiego/module05DynamicProgramming/module05ProgrammingAssignment01/020coinChangeDynamicProgram.kt
@@ -2,7 +2,7 @@ package coursera.ucSanDiego.module05DynamicProgramming.module05ProgrammingAssign
 
 fun main() {
 
-    fun minimumCoins(targetAmount: Int, coinDenominations: List<Int>): Int {
+    fun minimumCoins(targetAmount: Int, coinDenominations: List<Int>): Pair<Int, List<Int>> {
         // Create a cheat-sheet (Memoization, cache).
         val minCoins = IntArray(targetAmount + 1) { Int.MAX_VALUE }
         // We know the answer when the target amount is 0.
@@ -18,11 +18,27 @@ fun main() {
                 }
             }
         }
-        return minCoins[targetAmount]
+        // Backtracking (Reconstruction). Identify which coins we have used to change the target money.
+        // If this is not clear at this moment, checkout the next example:
+        // Primitive Calculator.
+        // https://github.com/sagarpatel288/kotlinDSAWithIntellijIdea/blob/f8687e7831a6788ed57387ab9be15a40dc2bb7a0/src/coursera/ucSanDiego/module05DynamicProgramming/module05ProgrammingAssignment01/030primitiveCalculator.kt
+        var indexPointer = targetAmount
+        val path = mutableListOf<Int>()
+        while (indexPointer > 0) {
+            for (option in coinDenominations) {
+                if (indexPointer - option >= 0 && minCoins[indexPointer] == minCoins[indexPointer - option] + 1) {
+                    path.add(option)
+                    indexPointer -= option
+                    break
+                }
+            }
+        }
+        return minCoins[targetAmount] to path.reversed()
     }
 
     val targetAmount = readln().toInt()
     val coinDenominations = listOf(1, 3, 4)
     val answer = minimumCoins(targetAmount, coinDenominations)
-    println(answer)
+    println(answer.first)
+    println(answer.second.joinToString(" "))
 }
