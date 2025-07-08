f582113f56ccb5fe947c43f803fc9010897e2eea
sagarpatel288 <sagarpatel288@gmail.com>
2025-07-08 17:54:35 +0530

    feat: implement bracket matching algorithm for code validation | 01checkBracketsInCode.kt | A part of the data structures. Reference: Coursera: UC San Diego: Data Structures: Module 01, Section 01 to 03 Arrays, Linked Lists, Stack, Queue, and Trees. | Programming Assignment - 01 | Problem-01.
    The commit adds a solution to check for properly matched brackets in code. It includes:
    - Data class for tracking bracket positions
    - Stack-based validation algorithm using ArrayDeque
    - Helper functions for bracket type checking
    - Detailed documentation of approach and complexity analysis

--- src/coursera/ucSanDiego/course02dataStructures/module01/section04assignmentProblems/01checkBracketsInCode.kt
+++ src/coursera/ucSanDiego/course02dataStructures/module01/section04assignmentProblems/01checkBracketsInCode.kt
@@ -180,7 +180,166 @@ package coursera.ucSanDiego.course02dataStructures.module01.section04assignmentP
  *
  * * `)` doesn’t match `[`, so `)` is the first unmatched closing bracket, so we output its position, which is 10.
  *
+ * # Thought-Process And Code Translations:
+ *
+ * ## Main Requirements:
+ *
+ * * If we find a mismatched closing bracket than the last opening bracket, then we need to return the position of this
+ * closing bracket.
+ * * If we find an unclosed opening bracket, then we need to return the position of this opening bracket.
+ * * Otherwise, we can print success.
+ *
+ * ## Meaning:
+ *
+ * * It means that, we need to track the position of the opening brackets and the closing brackets.
+ * * Each bracket has its own position.
+ * * So, we can bind bracket and its position with each other by creating a data class.
+ * * This way, it will be easy for us to get the position of each bracket.
+ * * So, we will create a data class. It may look like below:
+ * ```kotlin
+ * data class BracketWithPosition(val bracket: Char, val position: Int)
+ * ```
+ * * Now, the idea is to use a `stack` where everytime we get an opening bracket, we would add it to the `stack`.
+ * * If we get a closing bracket, we would pop the item from the stack, and we will compare it with the closing bracket.
+ * * For that, the stack must not be empty.
+ *
+ * * **If the stack is not empty:**
+ *
+ * * If they make a pair of `opening-closing` bracket, all good.
+ * * If they don't match, we have got a wrong closing bracket.
+ * * We need to return the position of this wrong closing bracket.
+ *
+ * * **If the stack is empty:**
+ *
+ * * If the parsed character is a closing bracket and the stack is empty.
+ * * It means that we don't have the relevant opening bracket to compare with.
+ * * We got a closing bracket before we get a relevant opening bracket.
+ * * It is an error.
+ * * So, we return the position of the closing bracket.
+ * * And if the parsed character is not a bracket, we don't want to do anything. We just continue the iteration.
+ *
+ * * The code will look like below:
+ * ```kotlin
+ *
+ * fun checkBracket(input: String): Int {
+ *     val arrayDeque = ArrayDeque<BracketWithPosition>()
+ *     for (i in 0..<input.length) {
+ *         if (isOpeningBracket(input[i])) {
+ *             arrayDeque.addLast(BracketWithPosition(input[i), i)
+ *         } else if (isClosingBracket(input[i])) && arrayDeque.isNotEmpty()) {
+ *             val poppedBracket = arrayDeque.removeLast()
+ *             if (!isProperPair(poppedBracket.bracket, input[i])) {
+ *                 return incoming.indexPosition + 1
+ *             }
+ *         } else if (isClosingBracket(input[i])) {
+ *             return incoming.indexPosition + 1
+ *         }
+ *     }
+ * }
+ *
+ * ```
+ *
+ * * In the end, if we find that our stack is empty, then it means that either we have removed all the opening brackets,
+ * and they have matched with the relevant closing brackets.
+ * * Because, if any of the item didn't match (get) the relevant closing bracket, we would have returned the position
+ * of the closing bracket.
+ * * So, we return `Success`.
+ * * Or, the other meaning of an empty stack is that there was no input string at all.
+ * * In that case, we want to return `Success` because we don't have position of any bracket, because there were no
+ * brackets at all!
+ * * Hence, in any case, in the end, if the stack is empty, we return `Success`.
+ * * But what if the stack is not empty?
+ * * Well, in that case, as we know that we add only opening brackets to the stack.
+ * * And we need to return the position of the first unmatched opening bracket.
+ * * So, we return the position of the first item.
+ * * But in a stack, we can't get the position of the first item until and unless we pop all the other items.
+ * * Yes, that is true.
+ * * And to make it easy, we can take a `Deque` - `A double ended queue` instead of a `stack`.
+ * * So, we would keep adding the elements from the back (rear) side as `deque.addLast()`.
+ * * And when we want to get the first item that is there in the container longer than any other item, we can simply
+ * use `deque.first()`.
+ * * Once we get the first item, we can simply return its position as `item.indexPosition + 1` because we have been
+ * asked to return the `1-based` index position.
+ * * So, this part translates into the following code:
+ *
+ * ```kotlin
+ *      return if (arrayDeque.isEmpty()) "Success" else arrayDeque.first().indexPosition + 1
+ * ```
+ * * But how can we return both `String` and `Int`?
+ * * Right, so we can change the return type of our function to be `String` and we can convert the `position` into a
+ * `String` before we return a position.
+ *
+ * ## Time Complexity:
+ *
+ * * We travel the entire string. So, it is `O(n)`.
+ *
+ * ## Space Complexity:
+ *
+ * * We take a `Deque` to store each bracket. In the worst case where all the characters are opening brackets only,
+ * it would be `O(n)`.
+ *
+ * ## ----------------------- Coursera's Grader Output -----------------------
+ *
+ * Good job! (Max time used: 0.15/2.00, max memory used: 50716672/536870912.)
+ *
+ *
  */
-fun checkBracketsInCode(input: String): Int {
+data class BracketWithPosition(val bracket: Char, val indexPosition: Int)
 
+fun isOpeningBracket(ch: Char): Boolean {
+    return ch == '[' || ch == '{' || ch == '('
+}
+
+fun isClosingBracket(ch: Char) = ch == ']' || ch == '}' || ch == ')'
+
+fun isProperPair(preExist: Char, new: Char): Boolean {
+    return when (preExist) {
+        '[' -> new == ']'
+        '{' -> new == '}'
+        '(' -> new == ')'
+        else -> false
+    }
+}
+
+fun checkBracketsInCode(input: String): String {
+    if (input.isEmpty()) return "Success"
+    // A deque - double ended queue to add and remove the items from back, and to get the first item from the front.
+    val deque = ArrayDeque<BracketWithPosition>()
+    for ((index, ch) in input.withIndex()) {
+        // If it is an opening bracket, add it to the deque.
+        if (isOpeningBracket(ch)) {
+            deque.addLast(BracketWithPosition(ch, index))
+        } else if (isClosingBracket(ch) && deque.isNotEmpty()) {
+            // We don't want to compare the opening bracket with any random character that is not a bracket.
+            // That is why the `isClosingBracket` condition.
+            // And we want to ensure that we don't try to remove an item from an empty deque.
+            // That is why the `deque.isNotEmpty` condition.
+            val popped = deque.removeLast()
+            // If the recently added opening bracket that we just have popped from the deque, doesn't match with the
+            // closing bracket, we have got a wrong closing bracket.
+            // If it is not a proper pair, return the index of the wrong closing bracket.
+            if (!isProperPair(popped.bracket, ch)) {
+                // We have been asked to return `1-based` index.
+                // That's why the `index + 1`.
+                return "${index + 1}"
+            }
+        } else if (isClosingBracket(ch)) {
+            // We don't want to return the index of any character that is not a bracket.
+            // That's why the `if` condition.
+            // Also, we have been asked to return `1-based` index.
+            // That's why the `index + 1`.
+            return "${index + 1}"
+        }
+    }
+    // If the deque is empty, all the opening bracket items of the deque have been successfully matched.
+    // So, return `Success`.
+    // Otherwise, return the first unmatched opening bracket position that has been waiting longer than any other item.
+    // We add `+1` because we have been asked to return `1-based` index.
+    return if (deque.isEmpty()) "Success" else "${deque.first().indexPosition + 1}"
+}
+
+
+fun main() {
+    val input = readln()
+    println(checkBracketsInCode(input))
 }
