1a7f3cef5b8ce14597b55c62bbb8d8d1330c2b41
sagarpatel288 <sagarpatel288@gmail.com>
2025-10-07 17:56:00 +0530

    docs: explain solution prerequisites and thought process for the longest common substring | 05longestCommonSubstring.kt | A part of the data structures. Reference: Coursera: UC San Diego:  Data Structures: Module 04: Hash Tables. | Programming Assignment: Problem-05: Longest Common Substring

--- src/courses/uc/course02dataStructures/module04hashTables/05longestCommonSubstring.kt
+++ src/courses/uc/course02dataStructures/module04hashTables/05longestCommonSubstring.kt
@@ -60,15 +60,53 @@ package courses.uc.course02dataStructures.module04hashTables
  *
  * #### Explanation
  *
- * * The longest common substring of the first pair of strings is `ool`, it starts at the first position in toolbox
- * and at the first position in cool.
+ * * The longest common substring of the first pair of strings is `ool`, it starts at the first position in `toolbox`
+ * and at the first position in `cool`.
  * * The strings from the second line do not share any non-empty common substrings
  * (in this case, 𝑙 = 0 and one may output any indices 𝑖 and 𝑗).
  * * Finally, the last two strings share a substring `aab` that has length `3` and starts at position `0` in the first
  * string and at position `4` in the second one.
  * * Note that for this pair of strings, one may output `2 3 3` as well.
  *
+ * ## Solution
  *
+ * ### Prerequisites
+ *
+ * * Binary Search
+ * * HashTables (Set, Map)
+ * * String Hashing
+ * * Rolling Hashing
+ *
+ * ### Thought Process
+ *
+ * * Suppose the length of `s` is `sl` and the length of `t` is `tl`.
+ * * Now, the maximum length of the common substring cannot be more than `minOf(sl, tl)`.
+ * * So, any substring of length between `0 to minOf(sl, tl)` can be a longest common substring.
+ * * But we don't need to process all the substrings of length from `0 to minOf(sl, tl)`.
+ * * Because, if a substring of length `k` is common, then there must exist a common substring of length <= `k`.
+ * * So, we don't need to check for any substrings whose length is less than or equal to `k`.
+ * * Because we are interested in the "Longest common substring."
+ * * For example, if `s = abcdefgh` and `t = cdefgh`.
+ * * Now, we know that the longest common substring length can be between `0 to minOf(sl, tl) = 0 to 6`.
+ * * Here, the lower-end is `0`, and the highest-end is `6`.
+ * * Let us assume that we start checking with the middle point `3`. The upper bound `6` remains the same.
+ * * We find that there exists a common substring of length `3`.
+ * * `cde`, `def`, `efg`, and `fgh`.
+ * * Now, we can clearly see that there are common substrings of length <= 3.
+ * * `cd`, `de`, `ef`, `fg`, and `gh`.
+ * * But we want to find the "Longest Common Substring".
+ * * When we find that there exists a common substring of length `3`, we increase the bar.
+ * * We ask: Can we have a common substring of length higher than 3?
+ * * Can we have a common substring of length 4?
+ * * We increase the lower end of the range.
+ * * We get a new range.
+ * * The new range becomes `4 to 6`.
+ * * Here, the lower-end is `4` and the higher-end is `6`.
+ * * Any substring of length between `k to minOf(sl, tl) = 4 to 6` can be a longest common substring.
+ * * Similarly, if there was no common substring of length `3`, we would decrease the `higher-end` of the range.
+ * * Because if there is no common substring of length `3`, then there cannot be a common substring of length > 3.
+ * * In that case, the higher-end would decrease to `mid-1 = 3 - 1 = 2` and the lower bound `0` remains the same.
+ * * So, in that case, the range would have become `0 to 2`.
  *
  */
 fun main() {
