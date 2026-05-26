c66dc8d42fbd9ec3003855bbdea46cd8049e6d78
sagarpatel288 <sagarpatel288@gmail.com>
2026-05-26 12:06:25 +0530

    doc(WIP): Add more comments in: 040primitiveCalculatorClean.kt
    Took 3 minutes

--- src/courses/uc/course01algorithmicToolbox/module05DynamicProgramming/module05ProgrammingAssignment01/040primitiveCalculatorClean.kt
+++ src/courses/uc/course01algorithmicToolbox/module05DynamicProgramming/module05ProgrammingAssignment01/040primitiveCalculatorClean.kt
@@ -6,13 +6,14 @@ package courses.uc.course01algorithmicToolbox.module05DynamicProgramming.module0
  */
 fun main() {
 
-    fun minOperationsWithStations(input: Int): Pair<Int, List<Int>> {
+    fun minOperationsWithStations(target: Int): Pair<Int, List<Int>> {
         // Container to store minimum operations for each number.
-        val operations = IntArray(input + 1)
-        // Base case.
+        // A container of `target + 1` size with default value `0`.
+        val operations = IntArray(target + 1)
+        // Base case: To reach `1`, we need `0` operations. Because `1` is the starting point.
         operations[1] = 0
         // Bottom-up.
-        for (i in 2..input) {
+        for (i in 2..target) {
             // This is always possible to reach `i` from `i - 1` using the `+1` operator.
             operations[i] = operations[i - 1] + 1
             // If `i` is divisible by `2`, we can reach `i` directly from `i/2` using the `*2` operator on `i/2`.
@@ -25,7 +26,7 @@ fun main() {
             }
         }
         // Backtracking.
-        var currentNumber = input
+        var currentNumber = target
         val path = mutableListOf<Int>()
         while (currentNumber > 1) {
             path.add(currentNumber)
@@ -41,6 +42,8 @@ fun main() {
                 }
             }
         }
+        // Caution! Possible point of mistake!
+        // We add `curr`, and not `1`. Maybe, to keep it dynamic.
         path.add(currentNumber)
         return operations.last() to path.reversed()
     }
