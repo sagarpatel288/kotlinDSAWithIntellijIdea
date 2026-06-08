b54342c65ac8e4717fa06f85481558e3f9e3c42d
sagarpatel288 <sagarpatel288@gmail.com>
2026-06-08 21:36:57 +0530

    doc(WIP): Add more comments to understand: 05totalInversions.kt
    Took 4 minutes

--- src/courses/uc/course01algorithmicToolbox/module04DivideAndConquerAssignment/05totalInversions.kt
+++ src/courses/uc/course01algorithmicToolbox/module04DivideAndConquerAssignment/05totalInversions.kt
@@ -141,6 +141,11 @@ fun main() {
             } else {
                 tempSorted[sortedPointer++] = input[rightPointer++]
                 // This is the key-point.
+                // Index of the left part is always going to be smaller than the index of the right part.
+                // So, all the cases where the item of the left part is greater than the item of the right part,
+                // is the case of inversion.
+                // Recall the definition of the inversion: if i < j, but a[i] > a[j], then it is the inversion.
+                // And:
                 // In a sorted array, if an element from the left side is greater than the element of the right side,
                 // all the other elements of the left side will also be greater than the element of the right side.
                 // For example, we know that we have two sorted parts.
