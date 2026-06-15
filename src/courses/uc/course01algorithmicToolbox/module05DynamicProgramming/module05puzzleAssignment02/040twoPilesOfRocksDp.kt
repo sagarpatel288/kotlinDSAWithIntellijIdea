7addc079adc4c41984786eaa6aa90eaf4a4f5c38
sagarpatel288 <sagarpatel288@gmail.com>
2026-06-15 14:31:14 +0530

    doc(WIP): Add more comments to understand: 040twoPilesOfRocksDp.kt
    Took 23 minutes

--- src/courses/uc/course01algorithmicToolbox/module05DynamicProgramming/module05puzzleAssignment02/040twoPilesOfRocksDp.kt
+++ src/courses/uc/course01algorithmicToolbox/module05DynamicProgramming/module05puzzleAssignment02/040twoPilesOfRocksDp.kt
@@ -21,14 +21,34 @@ class TwoPilesOfRocks {
                 // Caution! Possible point of mistake!
                 // We reset the `canWin` inside the inner `for` loop, just before our calculation.
                 var canWin = false
-                // A state [i, j] is a winning state if it can hand over a losing state.
+                // A state [i, j] is a winning state if it can hand over or end-up with being a losing state.
                 // If we can hand over a losing configuration, then the current state [i, j] is a winning state.
                 // If the configuration that we hand over after removing a rock from each pile is a losing state,
                 // then the current state [i, j] has a winning possibility for us.
+                // Another way to look at this is:
+                // If the current state [i, j] leads and ends-up with a losing state,
+                // it means that the current state [i, j] is a winning state.
+                // Because if we are handing over this current state [i, j] to the opponent,
+                // the opponent will have to modify it, and there is a possible move that can make the opponent lose
+                // the game.
+                // And if the opponent loses the game, we win the game.
+                // And it happened because we had this [i, j] state.
+                // That's why and that's how we say that it is a winning state (configuration).
+                // Because it forces the receiver of this state to make some changes and at least one of those
+                // changes can end-up being a losing state.
+                // We can think of this from the receiver's perspective, too.
+                // Suppose that we receive this [i, j] state.
+                // Now, it is our turn, and we have to make a move.
+                // If our move ends-up with being a losing state, it means that the configuration that we had
+                // received, pushed us towards the losing state, made us lose the game.
+                // That's why, we can say that the configuration we had received was a winning configuration.
+                // So, a configuration that can ends-up with being a losing configuration, is a winning configuration.
                 if (i > 0 && j > 0 && !dp[i - 1][j - 1]) canWin = true
+                // Similarly:
                 // If the configuration that we are handing over after removing a rock from the left pile,
                 // is a losing state, then the current state [i, j] is a winning state.
                 if (i > 0 && !dp[i - 1][j]) canWin = true
+                // And again, the same way:
                 // If the configuration that we are handing over after removing a rock from the right pile,
                 // is a losing state, then the current state [i, j] is a winning state.
                 if (j > 0 && !dp[i][j - 1]) canWin = true
