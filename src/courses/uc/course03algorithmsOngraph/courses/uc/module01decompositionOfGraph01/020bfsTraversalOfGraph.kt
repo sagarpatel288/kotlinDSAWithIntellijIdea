131cd8fa5ac4cd435e4165853ca7ce7fbd2882df
sagarpatel288 <sagarpatel288@gmail.com>
2026-08-23 16:20:23 +0530

    doc(WIP): Add comments to help us understand the 020bfsTraversalOfGraph.kt
    Took 11 minutes

--- src/courses/uc/course03algorithmsOngraph/courses/uc/module01decompositionOfGraph01/020bfsTraversalOfGraph.kt
+++ src/courses/uc/course03algorithmsOngraph/courses/uc/module01decompositionOfGraph01/020bfsTraversalOfGraph.kt
@@ -20,19 +20,35 @@ class BfsTraversalInGraph(val size: Int) {
         println(stringBuilder)
     }
 
-    fun bfsTraversal() {
+    /**
+     * * In a BST, we always start the BFS traversal from the root.
+     * * But in a graph, we can start the BFS Traversal from any vertex.
+     * * The thumb rule is that we first cover the immediate neighbor, no vertex should be repeated, and that no vertex should be left unattended.
+     */
+    fun bfsTraversal(start: Int) {
+        if (start !in adjacencyList.indices) return
         val stringBuilder = StringBuilder()
+        // We have to take a visited boolean array, because a graph can have a cycle.
+        // And we don't want to print a vertex more than once.
+        // And we don't want to keep running the program infinitely.
+        // That's why, we take this boolean array, and we mark the vertex as visited once we push it to the queue.
         val visited = BooleanArray(size) { false }
+        // We use a queue for the BFS Traversal.
         val queue = ArrayDeque<Int>()
-        queue.addLast(adjacencyList[0][0])
-        visited[adjacencyList[0][0]] = true
+        // We eagerly add (enqueue, push) one starting vertex to the queue, and mark it as visited.
+        queue.addLast(start)
+        // Once we add (enqueue, push) the vertex to the queue, we mark the vertex as visited.
+        visited[start] = true
         while (queue.isNotEmpty()) {
             val pop = queue.removeFirst()
             stringBuilder.append("$pop ")
+            // Add (enqueue) the neighbor list of the popped vertex.
             val neighborList = adjacencyList[pop]
             for (neighbor in neighborList) {
+                // Add (enqueue) only the unvisited vertices.
                 if (!visited[neighbor]) {
                     queue.addLast(neighbor)
+                    // Once we add (enqueue, push) the vertex to the queue, we mark the vertex as visited.
                     visited[neighbor] = true
                 }
             }
@@ -49,5 +65,5 @@ fun main() {
     graph.addEdges(2, 3)
     graph.addEdges(2, 4)
     graph.printAdjacencyList()
-    graph.bfsTraversal()
+    graph.bfsTraversal(0)
 }
