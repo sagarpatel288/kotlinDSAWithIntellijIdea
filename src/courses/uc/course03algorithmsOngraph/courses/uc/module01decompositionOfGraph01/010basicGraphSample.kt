3a02db254904fdc23a58d24c8d156d0259e79691
sagarpatel288 <sagarpatel288@gmail.com>
2026-08-23 13:09:49 +0530

    feat + doc: Add 010basicGraphSample.kt and 012simpleGraphOps.md to demonstrate the basic creation and printing of a Graph using the Shradha's lecture: https://youtu.be/RpgyCJBbl5E?si=0G0lTqP_34cW09uK
    Took 56 minutes

---
+++ src/courses/uc/course03algorithmsOngraph/courses/uc/module01decompositionOfGraph01/010basicGraphSample.kt
@@ -0,0 +1,33 @@
+package courses.uc.course03algorithmsOngraph.courses.uc.module01decompositionOfGraph01
+
+class Graph(val size: Int) {
+
+    val adjacencyList = List(size) { mutableListOf<Int>() }
+
+    fun addEdges(a: Int, b: Int) {
+        adjacencyList[a].add(b)
+        adjacencyList[b].add(a)
+    }
+
+    fun printAdjacencyList() {
+        val stringBuilder = StringBuilder()
+        for ((index, neighbors) in adjacencyList.withIndex()) {
+            stringBuilder.append("Vertex is $index: ")
+            neighbors.forEach {
+                stringBuilder.append(": $it ")
+            }
+            stringBuilder.append("\n")
+        }
+        println(stringBuilder)
+    }
+}
+
+fun main() {
+    val graph = Graph(5)
+    graph.addEdges(0, 1)
+    graph.addEdges(1, 2)
+    graph.addEdges(1, 3)
+    graph.addEdges(2, 3)
+    graph.addEdges(2, 4)
+    graph.printAdjacencyList()
+}
