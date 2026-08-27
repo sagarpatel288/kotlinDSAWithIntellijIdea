1b89e1c74bfbe4477a07dbd39226f9e7c2a56c64
sagarpatel288 <sagarpatel288@gmail.com>
2026-08-27 17:36:32 +0530

    correct and improve: Correct the documentation and implementation of count islands using DFS of an undirected graph | 032numberOfIslands.md, 040countIslands.kt
    Took 47 minutes

---
+++ src/courses/uc/course03algorithmsOngraph/courses/uc/module01decompositionOfGraph01/040countIslands.kt
@@ -0,0 +1,43 @@
+package courses.uc.course03algorithmsOngraph.courses.uc.module01decompositionOfGraph01
+
+/**
+ * # Reference
+ *
+ * * [Count islands](https://github.com/sagarpatel288/kotlinDSAWithIntellijIdea/blob/c985c009ef3e60eadecad784b50f976636dc5d13/docs/03graph/courses/uc/module01decompositionOfGraph01/032numberOfIslands.md)
+ *
+ * # Time Complexity
+ *
+ * * `O(rows * columns)`
+ *
+ * # Space Complexity
+ *
+ * * `O(rows * columns)`
+ */
+class CountIslands {
+
+    fun countIslands(grid: Array<CharArray>): Int {
+        // grid.size gives rows and grid[0].size gives columns
+        val visited = Array(grid.size) { BooleanArray(grid[0].size) { false } }
+        var islands = 0
+        for (i in 0..<grid.size) {
+            for (j in 0..<grid[0].size) {
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    dfs(i, j, grid, visited)
+                    islands++
+                }
+            }
+        }
+        return islands
+    }
+
+    private fun dfs(i: Int, j: Int, grid: Array<CharArray>, visited: Array<BooleanArray>) {
+        if (i !in 0..<grid.size || j !in 0..<(grid[0].size) || grid[i][j] == '0' || visited[i][j]) {
+            return
+        }
+        visited[i][j] = true
+        dfs(i - 1, j, grid, visited) // top
+        dfs(i, j + 1, grid, visited) // right
+        dfs(i + 1, j, grid, visited) // bottom
+        dfs(i, j - 1, grid, visited) // left
+    }
+}
